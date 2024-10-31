92b12e15d8c8b96fe03e6b196e3d3310eca9de40
LenDen1123 <sangwookpen00@gmail.com>
2024-10-31 16:21:14 +0900

    Initial commit

---
+++ src/main/java/com/example/spring1031/Spring1031Application.java
@@ -0,0 +1,13 @@
+package com.example.spring1031;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1031Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1031Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring1031/Spring1031ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring1031;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1031ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
